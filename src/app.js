6821df21bc55827059381d57b2a8ec6f597c7711
Bianca Ramos <biancakaroliner04@gmail.com>
2023-05-22 18:31:55 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,14 +15,22 @@ const selecoes = [
 
 function buscarSelecaoPorId(id){
     // retornar da seleções os dados filtrados - retornar o objeto por id
-    return selecoes.filter(selecao => selecao.id == id)
+    return selecoes.filter(selecao => selecao.id == id);
 }
 
 function BuscaIndexSelecao(id){
     // pegar a posição ou index do elemento no array por id
-    return selecoes.findIndex(selecao => selecao.id == id)
+    return selecoes.findIndex(selecao => selecao.id == id);
 }
 
+//POST - criar um novo ou adicionar
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body);
+    // 201 - sucesso na criação
+    res.status(201).send('Seleção cadastrada com sucesso');
+});
+
+//GET - acessar rotas
 //criar rota padrão ou raiz - GET
 app.get('/', (req, res) => {
     res.send('Hello word!');   //rota padrão de response(resposta)
@@ -33,20 +41,24 @@ app.get('/selecoes', (req, res) => {
 });
 
 app.get('/selecoes/:id', (req,res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
+    res.json(buscarSelecaoPorId(req.params.id));
 })
 
-app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body);
-    // 201 - sucesso na criação
-    res.status(201).send('Seleção cadastrada com sucesso');
+//PUT - atualizações e modificações de já existentes
+app.put('/selecoes/:id', (req, res) => {
+    let index = BuscaIndexSelecao(req.params.id);
+    //o parametro selecao vai ficar no lugar do conteudo da selecao de acordo com o index do array
+    selecoes[index].selecao = req.body.selecao;
+    selecoes[index].grupo = req.body.grupo;
+    res.json(selecoes);
 });
 
+//DELETE - deletar
 app.delete('/selecoes/:id', (req, res) => {
-    let index = BuscaIndexSelecao(req.params.id)
-    selecoes.splice(index, 1)
-    res.send(`seleção com id ${req.params.id} excuida com sucesso`)
-})
+    let index = BuscaIndexSelecao(req.params.id);
+    selecoes.splice(index, 1);
+    res.send(`seleção com id ${req.params.id} excuida com sucesso`);
+});
 
 //exportando a constante 
 export default app;
