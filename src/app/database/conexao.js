4300773593e3baba11e95a4a1a31ffe31be07983
Bianca Ramos <biancakaroliner04@gmail.com>
2023-06-13 13:44:25 -0300

    Node

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -10,4 +10,22 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+/**
+ * Executa um código sql com ou sem valores 
+ * @param {string} sql intrução sql a ser executada
+ * @param {string=id / [selecao, id]} valores a serem passados para o sql
+ * @param {string} mensagemReject mensagem a ser exibida
+ * @returns objeto da Promisse
+ */
+
+export const consulta = (sql, valores='', mensagemReject) => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, valores, (erro, resultado) => {
+            if(erro) return reject(mensagemReject)
+            const row = JSON.parse(JSON.stringify(resultado))
+            return resolve(row) 
+        })
+    })
+}
+
 export default conexao
